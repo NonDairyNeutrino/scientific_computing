c8ca01ecc1e0e0fa56f17c3d419a984333b19123
NonDairyNeutrino <chapmann19@gmail.com>
2024-02-01 09:08:27 -0800

    stuff

--- projects/prng/julia/prng.jl
+++ projects/prng/julia/prng.jl
@@ -1,25 +1,20 @@
+# TODO: add timing
+# TODO: add empirical-gradient descent
 include("benchmarks.jl")
-using Random, Statistics # standard library
-using .Benchmarks, Distributions # not in standard library
+include("stats.jl")
+using Random # standard library
+using .Benchmarks, .MyStats, Distributions # not in standard library
 
 matrixToVector(mat :: Matrix) = collect.(eachrow(mat))
 
-function doStats(v :: Vector)
-    average = mean(v)
-    stdDev  = std(v)
-    bounds  = extrema(v)
-    middle  = median(v)
-end
-
-function processBenchmark(benchmarkSymbol :: Symbol, prngs...)
+function processBenchmark(benchmarkSymbol :: Symbol, nVectors :: Int, dimension :: Int, prngs...)
     # get benchmark and its domain
     benchmark = Benchmarks.symbolToFunction[benchmarkSymbol]
     domain    = Benchmarks.benchmarkDomain[benchmarkSymbol]
-    # println("Starting $benchmark on thread $(Threads.threadid())")
     # generate random vectors within respective domains
     randomVectorVector = matrixToVector.(rand.(prngs, Uniform(domain...), nVectors, dimension))
     # pass the random vectors through the benchmark function
-    benchmark.(randomVectorVector)
+    return [benchmark.(randomVector) for randomVector in randomVectorVector]
     # println("Finished $benchmark")
 end
 
@@ -27,16 +22,26 @@ function main()
     # instantiate prngs
     rngXoshiro  = Xoshiro()
     rngMersenne = MersenneTwister()
+    rngVector   = [Xoshiro, MersenneTwister]
 
     # generate random vectors with different prngs
     # can go up to nVectors = 3*10^6, dimension = 30 with reasonable runtime
     nVectors  = 30
     dimension = 30
 
-    # Threads.@threads 
+    #= Threads.@threads =# 
     for benchmarkSymbol in names(Benchmarks)[2:end] # names() begins with the name of the package
-        processBenchmark(benchmarkSymbol, rngXoshiro, rngMersenne)
-        # Statistics
-
+        println("Starting $(string(benchmarkSymbol)) on thread $(Threads.threadid())")
+        processedBenchmarks = processBenchmark(benchmarkSymbol, nVectors, dimension, rngXoshiro, rngMersenne)
+        # println("Benchmark processing completed.")
+        # # Statistics
+        # println("Starting statistical analysis...")
+        stats = doStats.(processedBenchmarks)
+        for (rng, stats) in zip(rngVector, stats)
+            println(rng, " : ", stats)
+        end
+        println("=======================================================================")
     end
 end
+
+main()
