c598de086460a5186bc3218236c77e2ff6dcea3f
NonDairyNeutrino <chapmann19@gmail.com>
2024-02-03 22:24:30 -0800

    add rounding to stats

--- projects/prng/julia/stats.jl
+++ projects/prng/julia/stats.jl
@@ -4,10 +4,10 @@ export doStats
 using Statistics
 
 function doStats(v::Vector)
-    average = mean(v)
-    stdDev = std(v)
-    bounds = extrema(v)
-    middle = median(v)
+    average = round(mean(v),     sigdigits = 3)
+    stdDev  = round(std(v),      sigdigits = 3)
+    bounds  = round.(extrema(v), sigdigits = 3)
+    middle  = round(median(v),   sigdigits = 3)
     return (average = average, stdDev = stdDev, bounds = bounds, median = middle)
 end
 end
