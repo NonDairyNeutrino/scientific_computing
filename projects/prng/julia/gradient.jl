381449cff724e32adede5d1f4848dfa0b796c364
NonDairyNeutrino <chapmann19@gmail.com>
2024-02-03 16:11:11 -0800

    fix gradient

--- projects/prng/julia/gradient.jl
+++ projects/prng/julia/gradient.jl
@@ -6,16 +6,13 @@ export gradientDescent
 
 using LinearAlgebra
 
-function gradient(step :: T, foo :: Function, pointCurrent :: Vector) where T <: Real
-    # get the current image of the given point under foo
-    imageCurrent = foo(pointCurrent)
-
+function gradient(step :: T, foo :: Function, pointCurrent :: Vector, imageCurrent :: T) where T <: Real
     # create vector of copies of the input vector
     pointCurrentCopyVector = fill(pointCurrent, length(pointCurrent))
     # turn vector of vectors into matrixToVector to add scaled identity matrix
     pointNewMatrix         = stack(pointCurrentCopyVector) + step*I
     # turn back into vector of stepped-vectors
-    pointNewVector         = collect.(eachrow(pointNewMatrix))
+    pointNewVector         = collect.(eachcol(pointNewMatrix))
 
     # evaluate foo on each stepped point
     imageNewVector         = foo.(pointNewVector)
@@ -23,19 +20,24 @@ function gradient(step :: T, foo :: Function, pointCurrent :: Vector) where T <:
     return imageNewVector .- imageCurrent
 end
 
-function updatePoint(step :: T, foo :: Function, pointCurrent :: Vector) where T <: Real
-    grad = gradient(step, foo, pointCurrent)
-    return pointCurrent - step * grad, grad
+function updatePoint(step :: T, foo :: Function, pointCurrent :: Vector, imageCurrent :: T) where T <: Real
+    grad     = gradient(step, foo, pointCurrent, imageCurrent)
+    pointNew = pointCurrent - step * grad
+    return pointNew
 end
 
-function gradientDescent(step :: T, foo :: Function, pointCurrent :: Vector; threshold = 10^-10) where T <: Real
-    point = pointCurrent
-    grad  = [threshold]
-    while norm(grad) >= threshold
-        point, grad = updatePoint(step, foo, point)
+function gradientDescent(step :: T, foo :: Function, initialGuess :: Vector; threshold = 10) where T <: Real
+    point        = initialGuess
+    imageNew     = foo(initialGuess)
+    imageCurrent = imageNew + 1 # just to initialize and get into the loop
+
+    while imageNew < imageCurrent
+        imageCurrent = foo(point)
+        point        = updatePoint(step, foo, point, imageCurrent)
+        imageNew     = round(foo(point), digits = threshold)
     end
-    return point
+    return point, imageCurrent # <- optimizer, optimum
 end
-# gradientDescent(0.001, point -> dot(point, point), [1,0]) |> display
+# gradientDescent(0.01, point -> dot(point, point), [1.1, 200.]) |> display
 
 end
