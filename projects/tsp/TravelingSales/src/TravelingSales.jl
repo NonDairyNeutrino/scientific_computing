5512f7880c7462a0074dcfc8b572b77d016b5811
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-02-17 22:05:21 -0800

    add Solution

--- projects/tsp/TravelingSales/src/TravelingSales.jl
+++ projects/tsp/TravelingSales/src/TravelingSales.jl
@@ -2,6 +2,19 @@
 Main module to solve the Traveling Sales problem in both its symmetric and asymmetric forms.
 """
 module TravelingSales
-include("gravity.jl")
+"""
+    Solution
+
+Gives a structured object with mass, position, velocity, and acceleration.
+"""
+struct Solution
+    mass         :: Real
+    position     :: Vector
+    velocity     :: Vector
+    acceleration :: Vector
+    function Solution(position :: Vector)
+
+    end
+end
 
 end # module TravelingSales
