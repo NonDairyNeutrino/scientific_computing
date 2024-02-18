bd12c77794a789927e1fd8a709622e10fa8d1d07
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-02-18 10:18:30 -0800

    change gravity to use solution fields

--- projects/tsp/TravelingSales/src/gravity.jl
+++ projects/tsp/TravelingSales/src/gravity.jl
@@ -31,6 +31,6 @@ end
 
 Gives the attractive force between two solutions.
 """
-function gravity(bigG :: Real, mass1 :: Real, mass2 :: Real, distance :: Real, position1 :: Vector, position2 :: Vector; epsilon = 0.1, power = 1) :: Real
-    return bigG * mass1 * mass2 * (position2 - position1) / (distance^power + epsilon)
+function gravity(bigG :: Real, sol1 :: Solution, sol2 :: Solution; epsilon = 0.1, power = 1) :: Real
+    return bigG * sol1.mass * sol2.mass * (sol2.position - sol1.position) / (distance^power + epsilon)
 end
