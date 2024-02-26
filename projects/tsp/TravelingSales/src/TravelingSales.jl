5fb69383ada1a2f8a2f0e735284e157507ea92b0
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-02-26 09:23:52 -0800

    stuff

--- projects/tsp/TravelingSales/src/TravelingSales.jl
+++ projects/tsp/TravelingSales/src/TravelingSales.jl
@@ -34,41 +34,47 @@ function main(args :: Vector) :: Tuple{Vector{Int}, Float64}
     INITIALG   = 0.5 # 0.5 from literature
     FINALG     = 0.1 # 0.1 from literature
     GSTEP      = (FINALG - INITIALG) / MAXSTEPS    # linear decrease from literature
-    GVECTOR    = INITIALG .+ GSTEP .* (1:MAXSTEPS) # linear decrease from literature
+    GVECTOR    = INITIALG .+ GSTEP * (1:MAXSTEPS) # linear decrease from literature
 
     # INITIALIZE
     ## initialize population
     tsp            = Problem(parseProblem(DATAFILEPATH))
     DISTANCEMAX    = tsp.dimension - 1 # max distance is the max swaps needed to get from one to another
     fitness        = tourWeight(tsp.matrix)
+    localSearchFunction! = localSearch!(fitness)
     tourVector     = generateInitialPopulation(AGENTCOUNT, tsp.dimension)
     solutionVector = Solution.(0, tourVector, ceil.(Int, rand(Uniform(0, DISTANCEMAX), length(tourVector))), 0) # velocity is randomly initialized upon Solution creation
     setMasses!(fitness, solutionVector)
 
+    updateInterval = 50
     # CORE
     for step in 1:MAXSTEPS
-        step % 50 == 0 ? println("step: ", step) : nothing
+        step % updateInterval == 0 ? println("step: ", step) : nothing
+        step % updateInterval == 0 ? println("best fitness: ", fitness(argmax(solution -> solution.mass, solutionVector).position)) : nothing
 
         G                        = GVECTOR[step]
         gravityFunction          = gravity(DISTANCEMAX, G)
         accelerationFunction     = acceleration(gravityFunction)
         multiTargetMoveFunction! = multiTargetMove!(accelerationFunction)
 
-        K               = KVECTOR[step]
-        KBest           = sort(solutionVector; by = (solution -> solution.mass), rev = true)[1:K]
+        K     = KVECTOR[step]
+        KBest = sort(solutionVector; by = (solution -> solution.mass), rev = true)[1:K]
 
-        Threads.@threads for solution in solutionVector
+        for solution in solutionVector
             # DEPENDENT MOVEMENT OPERATOR
             multiTargetMoveFunction!(solution, KBest)
-        # INDEPENDENT MOVEMENT OPERATOR
-            localSearch(fitness, solution)
         end 
+
+        #= Threads.@threads  =#for solution in solutionVector
+            # INDEPENDENT MOVEMENT OPERATOR
+            localSearchFunction!(solution)
+        end
 
         # UPDATE MASSES
         setMasses!(fitness, solutionVector)
     end
 
     bestSolution = argmax(solution -> solution.mass, solutionVector)
-    return bestSolution.position, tourWeight(tsp.matrix, bestSolution.position)
+    return bestSolution.position, fitness(bestSolution.position)
 end
 end # module TravelingSales
