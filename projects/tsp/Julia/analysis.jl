a6f254262c784b058553db082007f3d3f6827623
NonDairyNeutrino <chapmann19@gmail.com>
2024-03-01 13:21:38 -0800

    add more to analysis

--- projects/tsp/Julia/analysis.jl
+++ projects/tsp/Julia/analysis.jl
@@ -1,6 +1,6 @@
 # Functionality to statistically significant analysis on the DGSA as applied to the tsp
 using Statistics, DelimitedFiles # from standard library
-include("TravelingSales/src/TravelingSales.jl")
+include("Julia/TravelingSales/src/TravelingSales.jl") # this path depends on your current working directory
 using .TravelingSales
 
 function doStats(v::Vector)
@@ -13,15 +13,24 @@ end
 
 # initialize data file
 dataFile = open("analysis_data.csv", "w")
+# create header
 writedlm(dataFile, [["name", "minimum", "maximum", "median", "average", "standard deviation"]], ", ")
 
 # create data
 const NUM_EXPERIMENTS = 30
-data = Vector(undef, NUM_EXPERIMENTS)
-Threads.@threads for i in 1:NUM_EXPERIMENTS
-    data[i] = main(ARGS)[2]
+const NUM_STEPS       = 200 # choose 200 as in literature
+const NUM_AGENTS      = 10  # choose 10 as in literature
+
+problemVector = readdir()
+statsMatrix = Matrix(undef, length(problemVector), 6#= number of stats as above e.g. name, minimum, etc. =#)
+
+Threads.@threads for (index, tsp) in enumerate(problemVector)
+    data = Vector(undef, NUM_EXPERIMENTS)
+    Threads.@threads for i in 1:NUM_EXPERIMENTS
+        data[i] = main([tsp, NUM_STEPS, NUM_AGENTS])[2]
+    end
+    statsMatrix[index, :] = doStats(data)
 end
-# do stats
-stats = doStats(data)
+
 # write data
 writedlm(dataFile, [[basename(ARGS[1]), stats...]], ", ")
