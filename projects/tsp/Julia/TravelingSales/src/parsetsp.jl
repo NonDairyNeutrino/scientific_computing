c90c9e05046a9f397a7658051766ae788341659a
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-03-01 09:39:29 -0800

    change to parse .xml files

--- projects/tsp/Julia/TravelingSales/src/parsetsp.jl
+++ projects/tsp/Julia/TravelingSales/src/parsetsp.jl
@@ -1,45 +1,33 @@
-# functionality to parse tsp files into adjacency matrices
-
-# NOTE: Geographic distance calculations
-#= 
-For converting coordinate input to longitude and latitude in radian:
-
-  PI = 3.141592;
-
-  deg = (int) x[i];
-  min = x[i]- deg;
-  rad = PI * (deg + 5.0 * min/ 3.0) / 180.0;
- 
-For computing the geographical distance:
-
- RRR = 6378.388;
-
- q1 = cos( longitude[i] - longitude[j] );
- q2 = cos( latitude[i] - latitude[j] );
- q3 = cos( latitude[i] + latitude[j] );
- dij = (int) ( RRR * acos( 0.5*((1.0+q1)*q2 - (1.0-q1)*q3) ) + 1.0); 
-=#
+# functionality to parse xml formatted tsp files into adjacency matrices
 
 """
     parseProblem(path :: String) :: Matrix
 
-Gives the adjacency matrix of the given TSP instance.
+Gives the adjacency matrix of the given TSP instance (.xml files)
 """
 function parseProblem(filePath :: String) :: Matrix
-    problemString = readchomp(filePath)
-    dimension = parse(Int, match(r"DIMENSION: (\d+)", problemString)[1])
+    println(filePath)
+    @assert splitext(filePath)[2] == ".xml" "Please give a .xml file"
 
-    if contains(problemString, "EDGE_WEIGHT_TYPE: EXPLICIT") && contains(problemString, "EDGE_WEIGHT_FORMAT: FULL_MATRIX")
-        regex = r"EDGE_WEIGHT_SECTION\n(?<weights>[\d\s]+)DISPLAY_DATA_SECTION"
-        weightString = match(regex, problemString)["weights"] |> strip
+    fileString = readchomp(filePath) # reads in the whole file as a string, problematic for large problems
+    regex = r"<edge cost=\"(?<weight>[\d.]+e[\+\-]\d+)\">(?<node>\d+)"
+    matchVector = eachmatch(regex, fileString) |> collect
 
-        mat = Matrix{Int}(undef, dimension, dimension)
-        for (i, row) in enumerate(eachmatch(r"[\d ]+", weightString))
-            for (j, col) in enumerate(eachmatch(r"\d+", row.match))
-                # println("row: $i, col: $j, n: ", col.match)
-                mat[i, j] = parse(Int, col.match)
-            end
-        end
+    dimension = maximum(parse(Int, match["node"]) for match in matchVector) + 1
+
+    adjacencyMatrix = zeros(dimension, dimension)
+    if matchVector[1]["node"] == "0"
+        row = 0
+    else
+        row = 1
     end
-    return mat
+    
+    for match in matchVector
+        if match["node"] == "0"
+            row += 1
+        end
+        col = parse(Int, match["node"]) + 1
+        adjacencyMatrix[row, col] = parse(Float64, match["weight"])
+    end
+    return adjacencyMatrix
 end
